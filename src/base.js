733aa631256e739fc48e1135ed904a92a96813c4
ChokSheak Lau <choksheak@gmail.com>
2020-05-30 15:27:06 -0700

    Update base.js

--- src/base.js
+++ src/base.js
@@ -1,8 +1,27 @@
-// Shared JS across the whole project.
+//
+//  base.js
+//
+//  Shared JS across the whole project.
+//  Usage: const base = require("./base").setWindow(window);
+//
 
 module.exports = class Base {
     static window;
-    static setWindow = w => { Base.window = w; return Base; };
-    static w = s => { Base.window.document.write(s); };
+
+    static wr = s => { Base.window.document.write(s); };
     static wl = s => { Base.window.document.writeln(s); };
+
+    static setWindow = window => {
+        // Set the window object needed by some functions in Base.
+        Base.window = window;
+
+        // Add all the functions in this class to the window object.
+        for (const key in Base) {
+            if (key !== 'window' && key !== 'setWindow') {
+                window[key] = Base[key];
+            }
+        }
+
+        return Base;
+    };
 }
